ad14c8c3d4b76b8018e6bd1ea3058be7f98d25c0
Robin Bijl <robin.bijl@tuni.fi>
2023-03-29 16:06:42 +0300

    AISA: port over pocl-android-sample from pocl
    example shows vector addition to be done on the GPU of the phone.

--- android/app/src/main/java/org/portablecl/poclaisademo/MainActivity.java
+++ android/app/src/main/java/org/portablecl/poclaisademo/MainActivity.java
@@ -9,10 +9,19 @@ import org.portablecl.poclaisademo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    // These native functions are defined in src/main/cpp/vectorAddExample.cpp
+    public native int initCL();
+    public native int vectorAddCL(int N, float[] A, float[] B, float[] C);
+    public native int destroyCL();
+    public native void setenv(String key, String value);
+
+
     // Used to load the 'poclaisademo' library on application startup.
     static {
         System.loadLibrary("poclaisademo");
     }
+
+    TextView text;
 
     private ActivityMainBinding binding;
 
@@ -26,6 +35,17 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+
+        text = binding.clOutput;
+        // Running in separate thread to avoid UI hangs
+        Thread td = new Thread() {
+            public void run() {
+                doVectorAdd();
+            }
+        };
+
+        td.start();
+
     }
 
     /**
@@ -33,4 +53,62 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    void doVectorAdd()
+    {
+        // Error checkings are not done for simplicity. Check logcat
+
+        printLog("\ncalling opencl init functions... ");
+        initCL();
+
+        // Create 2 vectors A & B
+        // And yes, this array size is embarrassingly huge for demo!
+        float A[] = {1, 2, 3, 4, 5, 6, 7};
+        float B[] = {8, 9, 0, 6, 7, 8, 9};
+        float C[] = new float[A.length];
+
+        printLog("\n A: ");
+        for(int i=0; i<A.length; i++)
+            printLog(Float.toString(A[i]) + "    ");
+
+        printLog("\n B: ");
+        for(int i=0; i<B.length; i++)
+            printLog(Float.toString(B[i]) + "    ");
+
+        printLog("\n\ncalling opencl vector-addition kernel... ");
+        vectorAddCL(C.length, A, B, C);
+
+        printLog("\n C: ");
+        for(int i=0; i<C.length; i++)
+            printLog(Float.toString(C[i]) + "    ");
+
+        boolean correct = true;
+        for(int i=0; i<C.length; i++)
+        {
+            if(C[i] != (A[i] + B[i])) {
+                correct = false;
+                break;
+            }
+        }
+
+        if(correct)
+            printLog("\n\nresult: passed\n");
+        else
+            printLog("\n\nresult: failed\n");
+
+        printLog("\ndestroy opencl resources... ");
+        destroyCL();
+    }
+
+
+    void printLog(final String str)
+    {
+        // UI updates should happen only in UI thread
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.append(str);
+            }
+        });
+    }
 }
