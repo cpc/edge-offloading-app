77de32c03a65d6620da09c91d5c8f5ee3592f17f
Robin Bijl <robin.bijl@tuni.fi>
2023-10-09 17:57:21 +0300

    cmake: move turbojpeg to external project

--- external/build_turbojpeg.bash
+++ external/build_turbojpeg.bash
@@ -19,49 +19,49 @@ rm -rf build
 mkdir build
 cd build
 
-# Build armv8
-echo "=== BUILDING armv8 ==="
-cmake -G"Ninja" \
-  -DANDROID_ABI=arm64-v8a \
-  -DANDROID_ARM_MODE=arm \
-  -DANDROID_PLATFORM=android-${ANDROID_VERSION} \
-  -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
-  -DCMAKE_ASM_FLAGS="--target=aarch64-linux-android${ANDROID_VERSION}" \
-  -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
-  ..
+# # Build armv8
+# echo "=== BUILDING armv8 ==="
+# cmake -G"Ninja" \
+#   -DANDROID_ABI=arm64-v8a \
+#   -DANDROID_ARM_MODE=arm \
+#   -DANDROID_PLATFORM=android-${ANDROID_VERSION} \
+#   -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
+#   -DCMAKE_ASM_FLAGS="--target=aarch64-linux-android${ANDROID_VERSION}" \
+#   -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
+#   ..
 
-ninja -j $(nproc)
+# ninja -j $(nproc)
 
-install_dir="${TJDIR}/libturbojpeg/arm64-v8a"
-mkdir -p $install_dir
-cp libturbojpeg.a $install_dir
+# install_dir="${TJDIR}/libturbojpeg/arm64-v8a"
+# mkdir -p $install_dir
+# cp libturbojpeg.a $install_dir
 
-cd ..
-rm -rf build
-mkdir build
-cd build
+# cd ..
+# rm -rf build
+# mkdir build
+# cd build
 
-# Build armv7
-echo "=== BUILDING armv7 ==="
-cmake -G"Ninja" \
-  -DANDROID_ABI=armeabi-v7a \
-  -DANDROID_ARM_MODE=arm \
-  -DANDROID_PLATFORM=android-${ANDROID_VERSION} \
-  -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
-  -DCMAKE_ASM_FLAGS="--target=arm-linux-androideabi${ANDROID_VERSION}" \
-  -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
-  ..
+# # Build armv7
+# echo "=== BUILDING armv7 ==="
+# cmake -G"Ninja" \
+#   -DANDROID_ABI=armeabi-v7a \
+#   -DANDROID_ARM_MODE=arm \
+#   -DANDROID_PLATFORM=android-${ANDROID_VERSION} \
+#   -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
+#   -DCMAKE_ASM_FLAGS="--target=arm-linux-androideabi${ANDROID_VERSION}" \
+#   -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
+#   ..
 
-ninja -j $(nproc)
+# ninja -j $(nproc)
 
-install_dir="${TJDIR}/libturbojpeg/armeabi-v7a"
-mkdir -p $install_dir
-cp libturbojpeg.a $install_dir
+# install_dir="${TJDIR}/libturbojpeg/armeabi-v7a"
+# mkdir -p $install_dir
+# cp libturbojpeg.a $install_dir
 
-cd ..
-rm -rf build
-mkdir build
-cd build
+# cd ..
+# rm -rf build
+# mkdir build
+# cd build
 
 # Build x86-64
 echo "=== BUILDING x86-64 ==="
