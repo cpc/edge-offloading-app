20c0e510fdf6ab233a966ba177d3a5daf554afaf
Jakub Zadnik <jakub.zadnik@tuni.fi>
2024-05-06 12:10:28 +0300

    Update pocl; Print device BiKs on startup

--- android/app/src/main/cpp/poclImageProcessorV2.cpp
+++ android/app/src/main/cpp/poclImageProcessorV2.cpp
@@ -361,14 +361,16 @@ create_pocl_image_processor_context(pocl_image_processor_context **ret_ctx, cons
     ctx->devices_found = devices_found;
 
     // some info
-    char result_array[256];
+    char result_array[1024];
     for (unsigned i = 0; i < devices_found; ++i) {
-        clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 256 * sizeof(char), result_array, NULL);
+        clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 1024 * sizeof(char), result_array, NULL);
         LOGI("device %d: CL_DEVICE_NAME:    %s\n", i, result_array);
-        clGetDeviceInfo(devices[i], CL_DEVICE_VERSION, 256 * sizeof(char), result_array, NULL);
+        clGetDeviceInfo(devices[i], CL_DEVICE_VERSION, 1024 * sizeof(char), result_array, NULL);
         LOGI("device %d: CL_DEVICE_VERSION: %s\n", i, result_array);
-        clGetDeviceInfo(devices[i], CL_DRIVER_VERSION, 256 * sizeof(char), result_array, NULL);
+        clGetDeviceInfo(devices[i], CL_DRIVER_VERSION, 1024 * sizeof(char), result_array, NULL);
         LOGI("device %d: CL_DRIVER_VERSION: %s\n", i, result_array);
+        clGetDeviceInfo(devices[i], CL_DEVICE_BUILT_IN_KERNELS, 1024 * sizeof(char), result_array, NULL);
+        LOGI("device %d: CL_DRIVER_BUILT_IN_KERNELS: %s\n", i, result_array);
     }
 
     cl_context_properties cps[] = {CL_CONTEXT_PLATFORM, (cl_context_properties) platform,
